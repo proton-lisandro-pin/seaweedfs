6f1a58679e9f69c9216ba761c74e7a1f49cf0d7f
Lisandro Pin <lisandro.pin@proton.ch>
2025-06-04 02:45:51 +0200

    Q&D test of a `CompactMap()` implementation using vanilla golang maps.

--- weed/storage/needle_map/compact_map.go
+++ weed/storage/needle_map/compact_map.go
@@ -1,42 +1,24 @@
 package needle_map
 
 import (
+	"cmp"
 	"fmt"
-	"math"
-	"sort"
+	"slices"
 	"sync"
 
 	"github.com/seaweedfs/seaweedfs/weed/storage/types"
 )
 
-const (
-	MaxCompactKey    = math.MaxUint16
-	SegmentChunkSize = 50000 // should be <= MaxCompactKey
-)
-
-type CompactKey uint16
 type CompactOffset [types.OffsetSize]byte
 type CompactNeedleValue struct {
-	key    CompactKey
 	offset CompactOffset
 	size   types.Size
-}
-
-type CompactMapSegment struct {
-	list     []CompactNeedleValue
-	chunk    int
-	firstKey CompactKey
-	lastKey  CompactKey
 }
 
 type CompactMap struct {
 	sync.RWMutex
 
-	segments map[int]*CompactMapSegment
-}
-
-func (ck CompactKey) Key(chunk int) types.NeedleId {
-	return (types.NeedleId(SegmentChunkSize) * types.NeedleId(chunk)) + types.NeedleId(ck)
+	values map[types.NeedleId]CompactNeedleValue
 }
 
 func OffsetToCompact(offset types.Offset) CompactOffset {
@@ -49,169 +31,27 @@ func (co CompactOffset) Offset() types.Offset {
 	return types.BytesToOffset(co[:])
 }
 
-func (cnv CompactNeedleValue) NeedleValue(chunk int) NeedleValue {
-	key := cnv.key.Key(chunk)
+func NewCompactMap() *CompactMap {
+	return &CompactMap{
+		values: map[types.NeedleId]CompactNeedleValue{},
+	}
+}
+
+func (cm *CompactMap) Len() int {
+	return len(cm.values)
+}
+
+func (cm *CompactMap) String() string {
+	return fmt.Sprintf("%d elements", cm.Len())
+}
+
+func (cm *CompactMap) needleValue(key types.NeedleId) NeedleValue {
+	cnv := cm.values[key]
 	return NeedleValue{
 		Key:    key,
 		Offset: cnv.offset.Offset(),
 		Size:   cnv.size,
 	}
-}
-
-func newCompactMapSegment(chunk int) *CompactMapSegment {
-	return &CompactMapSegment{
-		list:     []CompactNeedleValue{},
-		chunk:    chunk,
-		firstKey: MaxCompactKey,
-		lastKey:  0,
-	}
-}
-
-func (cs *CompactMapSegment) len() int {
-	return len(cs.list)
-}
-
-func (cs *CompactMapSegment) cap() int {
-	return cap(cs.list)
-}
-
-func (cs *CompactMapSegment) compactKey(key types.NeedleId) CompactKey {
-	return CompactKey(key - (types.NeedleId(SegmentChunkSize) * types.NeedleId(cs.chunk)))
-}
-
-// bsearchKey returns the CompactNeedleValue index for a given ID key.
-// If the key is not found, it returns the index where it should be inserted instead.
-func (cs *CompactMapSegment) bsearchKey(key types.NeedleId) (int, bool) {
-	ck := cs.compactKey(key)
-
-	switch {
-	case len(cs.list) == 0:
-		return 0, false
-	case ck == cs.firstKey:
-		return 0, true
-	case ck <= cs.firstKey:
-		return 0, false
-	case ck == cs.lastKey:
-		return len(cs.list) - 1, true
-	case ck > cs.lastKey:
-		return len(cs.list), false
-	}
-
-	i := sort.Search(len(cs.list), func(i int) bool {
-		return cs.list[i].key >= ck
-	})
-	return i, cs.list[i].key == ck
-}
-
-// set inserts/updates a CompactNeedleValue.
-// If the operation is an update, returns the overwritten value's previous offset and size.
-func (cs *CompactMapSegment) set(key types.NeedleId, offset types.Offset, size types.Size) (oldOffset types.Offset, oldSize types.Size) {
-	i, found := cs.bsearchKey(key)
-	if found {
-		// update
-		o := cs.list[i].offset.Offset()
-		oldOffset.OffsetLower = o.OffsetLower
-		oldOffset.OffsetHigher = o.OffsetHigher
-		oldSize = cs.list[i].size
-
-		o.OffsetLower = offset.OffsetLower
-		o.OffsetHigher = offset.OffsetHigher
-		cs.list[i].offset = OffsetToCompact(o)
-		cs.list[i].size = size
-		return
-	}
-
-	// insert
-	if len(cs.list) >= SegmentChunkSize {
-		panic(fmt.Sprintf("attempted to write more than %d entries on CompactMapSegment %p!!!", SegmentChunkSize, cs))
-	}
-	if len(cs.list) == SegmentChunkSize-1 {
-		// if we max out our segment storage, pin its capacity to minimize memory usage
-		nl := make([]CompactNeedleValue, SegmentChunkSize, SegmentChunkSize)
-		copy(nl, cs.list[:i])
-		copy(nl[i+1:], cs.list[i:])
-		cs.list = nl
-	} else {
-		cs.list = append(cs.list, CompactNeedleValue{})
-		copy(cs.list[i+1:], cs.list[i:])
-	}
-
-	ck := cs.compactKey(key)
-	cs.list[i] = CompactNeedleValue{
-		key:    ck,
-		offset: OffsetToCompact(offset),
-		size:   size,
-	}
-	if ck < cs.firstKey {
-		cs.firstKey = ck
-	}
-	if ck > cs.lastKey {
-		cs.lastKey = ck
-	}
-
-	return
-}
-
-// get seeks a map entry by key. Returns an entry pointer, with a boolean specifiying if the entry was found.
-func (cs *CompactMapSegment) get(key types.NeedleId) (*CompactNeedleValue, bool) {
-	if i, found := cs.bsearchKey(key); found {
-		return &cs.list[i], true
-	}
-
-	return nil, false
-}
-
-// delete deletes a map entry by key. Returns the entries' previous Size, if available.
-func (cs *CompactMapSegment) delete(key types.NeedleId) types.Size {
-	if i, found := cs.bsearchKey(key); found {
-		if cs.list[i].size > 0 && cs.list[i].size.IsValid() {
-			ret := cs.list[i].size
-			cs.list[i].size = -cs.list[i].size
-			return ret
-		}
-	}
-
-	return types.Size(0)
-}
-
-func NewCompactMap() *CompactMap {
-	return &CompactMap{
-		segments: map[int]*CompactMapSegment{},
-	}
-}
-
-func (cm *CompactMap) Len() int {
-	l := 0
-	for _, s := range cm.segments {
-		l += s.len()
-	}
-	return l
-}
-
-func (cm *CompactMap) Cap() int {
-	c := 0
-	for _, s := range cm.segments {
-		c += s.cap()
-	}
-	return c
-}
-
-func (cm *CompactMap) String() string {
-	return fmt.Sprintf(
-		"%d/%d elements on %d segments, %.02f%% efficiency",
-		cm.Len(), cm.Cap(), len(cm.segments),
-		float64(100)*float64(cm.Len())/float64(cm.Cap()))
-}
-
-func (cm *CompactMap) segmentForKey(key types.NeedleId) *CompactMapSegment {
-	chunk := int(key / SegmentChunkSize)
-	if cs, ok := cm.segments[chunk]; ok {
-		return cs
-	}
-
-	cs := newCompactMapSegment(chunk)
-	cm.segments[chunk] = cs
-	return cs
 }
 
 // Set inserts/updates a NeedleValue.
@@ -220,8 +60,19 @@ func (cm *CompactMap) Set(key types.NeedleId, offset types.Offset, size types.Si
 	cm.RLock()
 	defer cm.RUnlock()
 
-	cs := cm.segmentForKey(key)
-	return cs.set(key, offset, size)
+	if nv, ok := cm.values[key]; ok {
+		o := nv.offset.Offset()
+		oldOffset.OffsetLower = o.OffsetLower
+		oldOffset.OffsetHigher = o.OffsetHigher
+		oldSize = nv.size
+	}
+
+	cm.values[key] = CompactNeedleValue{
+		offset: OffsetToCompact(offset),
+		size:   size,
+	}
+
+	return
 }
 
 // Get seeks a map entry by key. Returns an entry pointer, with a boolean specifiying if the entry was found.
@@ -229,11 +80,11 @@ func (cm *CompactMap) Get(key types.NeedleId) (*NeedleValue, bool) {
 	cm.RLock()
 	defer cm.RUnlock()
 
-	cs := cm.segmentForKey(key)
-	if cnv, found := cs.get(key); found {
-		nv := cnv.NeedleValue(cs.chunk)
+	if _, ok := cm.values[key]; ok {
+		nv := cm.needleValue(key)
 		return &nv, true
 	}
+
 	return nil, false
 }
 
@@ -242,8 +93,17 @@ func (cm *CompactMap) Delete(key types.NeedleId) types.Size {
 	cm.RLock()
 	defer cm.RUnlock()
 
-	cs := cm.segmentForKey(key)
-	return cs.delete(key)
+	if cnv, ok := cm.values[key]; ok {
+		if cnv.size > 0 && cnv.size.IsValid() {
+			cm.values[key] = CompactNeedleValue{
+				offset: cnv.offset,
+				size:   -cnv.size,
+			}
+			return cnv.size
+		}
+	}
+
+	return types.Size(0)
 }
 
 // AscendingVisit runs a function on all entries, in ascending key order. Returns any errors hit while visiting.
@@ -251,19 +111,18 @@ func (cm *CompactMap) AscendingVisit(visit func(NeedleValue) error) error {
 	cm.RLock()
 	defer cm.RUnlock()
 
-	chunks := []int{}
-	for c := range cm.segments {
-		chunks = append(chunks, c)
+	keys := []types.NeedleId{}
+	for key := range cm.values {
+		keys = append(keys, key)
 	}
-	sort.Ints(chunks)
+	slices.SortFunc(keys, func(a, b types.NeedleId) int {
+		return cmp.Compare(uint64(a), uint64(b))
+	})
 
-	for _, c := range chunks {
-		cs := cm.segments[c]
-		for _, cnv := range cs.list {
-			nv := cnv.NeedleValue(cs.chunk)
-			if err := visit(nv); err != nil {
-				return err
-			}
+	for _, k := range keys {
+		nv := cm.needleValue(k)
+		if err := visit(nv); err != nil {
+			return err
 		}
 	}
 	return nil
--- weed/storage/needle_map/compact_map_test.go
+++ weed/storage/needle_map/compact_map_test.go
@@ -9,127 +9,17 @@ import (
 	"github.com/seaweedfs/seaweedfs/weed/storage/types"
 )
 
-func TestSegmentBsearchKey(t *testing.T) {
-	testSegment := &CompactMapSegment{
-		list: []CompactNeedleValue{
-			CompactNeedleValue{key: 10},
-			CompactNeedleValue{key: 20},
-			CompactNeedleValue{key: 21},
-			CompactNeedleValue{key: 26},
-			CompactNeedleValue{key: 30},
-		},
-		firstKey: 10,
-		lastKey:  30,
-	}
-
-	testCases := []struct {
-		name      string
-		cs        *CompactMapSegment
-		key       types.NeedleId
-		wantIndex int
-		wantFound bool
-	}{
-		{
-			name:      "empty segment",
-			cs:        newCompactMapSegment(0),
-			key:       123,
-			wantIndex: 0,
-			wantFound: false,
-		},
-		{
-			name:      "new key, insert at beggining",
-			cs:        testSegment,
-			key:       5,
-			wantIndex: 0,
-			wantFound: false,
-		},
-		{
-			name:      "new key, insert at end",
-			cs:        testSegment,
-			key:       100,
-			wantIndex: 5,
-			wantFound: false,
-		},
-		{
-			name:      "new key, insert second",
-			cs:        testSegment,
-			key:       12,
-			wantIndex: 1,
-			wantFound: false,
-		},
-		{
-			name:      "new key, insert in middle",
-			cs:        testSegment,
-			key:       23,
-			wantIndex: 3,
-			wantFound: false,
-		},
-		{
-			name:      "key #1",
-			cs:        testSegment,
-			key:       10,
-			wantIndex: 0,
-			wantFound: true,
-		},
-		{
-			name:      "key #2",
-			cs:        testSegment,
-			key:       20,
-			wantIndex: 1,
-			wantFound: true,
-		},
-		{
-			name:      "key #3",
-			cs:        testSegment,
-			key:       21,
-			wantIndex: 2,
-			wantFound: true,
-		},
-		{
-			name:      "key #4",
-			cs:        testSegment,
-			key:       26,
-			wantIndex: 3,
-			wantFound: true,
-		},
-		{
-			name:      "key #5",
-			cs:        testSegment,
-			key:       30,
-			wantIndex: 4,
-			wantFound: true,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			index, found := tc.cs.bsearchKey(tc.key)
-			if got, want := index, tc.wantIndex; got != want {
-				t.Errorf("expected %v, got %v", want, got)
-			}
-			if got, want := found, tc.wantFound; got != want {
-				t.Errorf("expected %v, got %v", want, got)
-			}
-		})
-	}
-}
-
 func TestSegmentSet(t *testing.T) {
-	testSegment := &CompactMapSegment{
-		list: []CompactNeedleValue{
-			CompactNeedleValue{key: 10, offset: OffsetToCompact(types.Uint32ToOffset(0)), size: 100},
-			CompactNeedleValue{key: 20, offset: OffsetToCompact(types.Uint32ToOffset(100)), size: 200},
-			CompactNeedleValue{key: 30, offset: OffsetToCompact(types.Uint32ToOffset(300)), size: 300},
+	testMap := &CompactMap{
+		values: map[types.NeedleId]CompactNeedleValue{
+			10: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(0)), size: 100},
+			20: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(100)), size: 200},
+			30: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(300)), size: 300},
 		},
-		firstKey: 10,
-		lastKey:  30,
 	}
 
-	if got, want := testSegment.len(), 3; got != want {
+	if got, want := testMap.Len(), 3; got != want {
 		t.Errorf("got starting size %d, want %d", got, want)
-	}
-	if got, want := testSegment.cap(), 3; got != want {
-		t.Errorf("got starting capacity %d, want %d", got, want)
 	}
 
 	testSets := []struct {
@@ -163,7 +53,7 @@ func TestSegmentSet(t *testing.T) {
 	}
 
 	for _, ts := range testSets {
-		offset, size := testSegment.set(ts.key, ts.offset, ts.size)
+		offset, size := testMap.Set(ts.key, ts.offset, ts.size)
 		if offset != ts.wantOffset {
 			t.Errorf("%s: got offset %v, want %v", ts.name, offset, ts.wantOffset)
 		}
@@ -172,68 +62,38 @@ func TestSegmentSet(t *testing.T) {
 		}
 	}
 
-	wantSegment := &CompactMapSegment{
-		list: []CompactNeedleValue{
-			CompactNeedleValue{key: 5, offset: OffsetToCompact(types.Uint32ToOffset(1000)), size: 123},
-			CompactNeedleValue{key: 10, offset: OffsetToCompact(types.Uint32ToOffset(0)), size: 100},
-			CompactNeedleValue{key: 20, offset: OffsetToCompact(types.Uint32ToOffset(100)), size: 200},
-			CompactNeedleValue{key: 25, offset: OffsetToCompact(types.Uint32ToOffset(8000)), size: 789},
-			CompactNeedleValue{key: 30, offset: OffsetToCompact(types.Uint32ToOffset(9000)), size: 999},
-			CompactNeedleValue{key: 51, offset: OffsetToCompact(types.Uint32ToOffset(7000)), size: 456},
+	wantMap := &CompactMap{
+		values: map[types.NeedleId]CompactNeedleValue{
+			5:  CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(1000)), size: 123},
+			10: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(0)), size: 100},
+			20: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(100)), size: 200},
+			25: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(8000)), size: 789},
+			30: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(9000)), size: 999},
+			51: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(7000)), size: 456},
 		},
-		firstKey: 5,
-		lastKey:  51,
 	}
-	if !reflect.DeepEqual(testSegment, wantSegment) {
-		t.Errorf("got result segment %v, want %v", testSegment, wantSegment)
+	if !reflect.DeepEqual(testMap, wantMap) {
+		t.Errorf("got result segment %v, want %v", testMap, wantMap)
 	}
 
-	if got, want := testSegment.len(), 6; got != want {
+	if got, want := testMap.Len(), 6; got != want {
 		t.Errorf("got result size %d, want %d", got, want)
-	}
-	if got, want := testSegment.cap(), 6; got != want {
-		t.Errorf("got result capacity %d, want %d", got, want)
-	}
-}
-
-func TestSegmentSetOrdering(t *testing.T) {
-	keys := []types.NeedleId{}
-	for i := 0; i < SegmentChunkSize; i++ {
-		keys = append(keys, types.NeedleId(i))
-	}
-
-	r := rand.New(rand.NewSource(123456789))
-	r.Shuffle(len(keys), func(i, j int) { keys[i], keys[j] = keys[j], keys[i] })
-
-	cs := newCompactMapSegment(0)
-	for _, k := range keys {
-		_, _ = cs.set(k, types.Uint32ToOffset(123), 456)
-	}
-	if got, want := cs.len(), SegmentChunkSize; got != want {
-		t.Errorf("expected size %d, got %d", want, got)
-	}
-	for i := 1; i < cs.len(); i++ {
-		if ka, kb := cs.list[i-1].key, cs.list[i].key; ka >= kb {
-			t.Errorf("found out of order entries at (%d, %d) = (%d, %d)", i-1, i, ka, kb)
-		}
 	}
 }
 
 func TestSegmentGet(t *testing.T) {
-	testSegment := &CompactMapSegment{
-		list: []CompactNeedleValue{
-			CompactNeedleValue{key: 10, offset: OffsetToCompact(types.Uint32ToOffset(0)), size: 100},
-			CompactNeedleValue{key: 20, offset: OffsetToCompact(types.Uint32ToOffset(100)), size: 200},
-			CompactNeedleValue{key: 30, offset: OffsetToCompact(types.Uint32ToOffset(300)), size: 300},
+	testMap := &CompactMap{
+		values: map[types.NeedleId]CompactNeedleValue{
+			10: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(0)), size: 100},
+			20: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(100)), size: 200},
+			30: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(300)), size: 300},
 		},
-		firstKey: 10,
-		lastKey:  30,
 	}
 
 	testCases := []struct {
 		name      string
 		key       types.NeedleId
-		wantValue *CompactNeedleValue
+		wantValue *NeedleValue
 		wantFound bool
 	}{
 		{
@@ -245,46 +105,50 @@ func TestSegmentGet(t *testing.T) {
 		{
 			name:      "key #1",
 			key:       10,
-			wantValue: &testSegment.list[0],
+			wantValue: &NeedleValue{Key: 10, Offset: types.Uint32ToOffset(0), Size: 100},
 			wantFound: true,
 		},
 		{
 			name:      "key #2",
 			key:       20,
-			wantValue: &testSegment.list[1],
+			wantValue: &NeedleValue{Key: 20, Offset: types.Uint32ToOffset(100), Size: 200},
 			wantFound: true,
 		},
 		{
 			name:      "key #3",
 			key:       30,
-			wantValue: &testSegment.list[2],
+			wantValue: &NeedleValue{Key: 30, Offset: types.Uint32ToOffset(300), Size: 300},
 			wantFound: true,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			value, found := testSegment.get(tc.key)
-			if got, want := value, tc.wantValue; got != want {
-				t.Errorf("got %v, want %v", got, want)
-			}
+			value, found := testMap.Get(tc.key)
 			if got, want := found, tc.wantFound; got != want {
 				t.Errorf("got %v, want %v", got, want)
+			}
+			if tc.wantValue != nil {
+				if got, want := *value, *tc.wantValue; got != want {
+					t.Errorf("got %v, want %v", got, want)
+				}
+			} else {
+				if value != nil {
+					t.Errorf("got %v, want nil", value)
+				}
 			}
 		})
 	}
 }
 
 func TestSegmentDelete(t *testing.T) {
-	testSegment := &CompactMapSegment{
-		list: []CompactNeedleValue{
-			CompactNeedleValue{key: 10, offset: OffsetToCompact(types.Uint32ToOffset(0)), size: 100},
-			CompactNeedleValue{key: 20, offset: OffsetToCompact(types.Uint32ToOffset(100)), size: 200},
-			CompactNeedleValue{key: 30, offset: OffsetToCompact(types.Uint32ToOffset(300)), size: 300},
-			CompactNeedleValue{key: 40, offset: OffsetToCompact(types.Uint32ToOffset(600)), size: 400},
+	testMap := &CompactMap{
+		values: map[types.NeedleId]CompactNeedleValue{
+			10: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(0)), size: 100},
+			20: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(100)), size: 200},
+			30: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(300)), size: 300},
+			40: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(600)), size: 400},
 		},
-		firstKey: 10,
-		lastKey:  40,
 	}
 
 	testDeletes := []struct {
@@ -310,100 +174,22 @@ func TestSegmentDelete(t *testing.T) {
 	}
 
 	for _, td := range testDeletes {
-		size := testSegment.delete(td.key)
+		size := testMap.Delete(td.key)
 		if got, want := size, td.want; got != want {
 			t.Errorf("%s: got %v, want %v", td.name, got, want)
 		}
 	}
 
-	wantSegment := &CompactMapSegment{
-		list: []CompactNeedleValue{
-			CompactNeedleValue{key: 10, offset: OffsetToCompact(types.Uint32ToOffset(0)), size: 100},
-			CompactNeedleValue{key: 20, offset: OffsetToCompact(types.Uint32ToOffset(100)), size: -200},
-			CompactNeedleValue{key: 30, offset: OffsetToCompact(types.Uint32ToOffset(300)), size: 300},
-			CompactNeedleValue{key: 40, offset: OffsetToCompact(types.Uint32ToOffset(600)), size: -400},
-		},
-		firstKey: 10,
-		lastKey:  40,
-	}
-	if !reflect.DeepEqual(testSegment, wantSegment) {
-		t.Errorf("got result segment %v, want %v", testSegment, wantSegment)
-	}
-}
-
-func TestSegmentForKey(t *testing.T) {
-	testMap := NewCompactMap()
-
-	tests := []struct {
-		name string
-		key  types.NeedleId
-		want *CompactMapSegment
-	}{
-		{
-			name: "first segment",
-			key:  12,
-			want: &CompactMapSegment{
-				list:     []CompactNeedleValue{},
-				chunk:    0,
-				firstKey: MaxCompactKey,
-				lastKey:  0,
-			},
-		},
-		{
-			name: "second segment, gapless",
-			key:  SegmentChunkSize + 34,
-			want: &CompactMapSegment{
-				list:     []CompactNeedleValue{},
-				chunk:    1,
-				firstKey: MaxCompactKey,
-				lastKey:  0,
-			},
-		},
-		{
-			name: "gapped segment",
-			key:  (5 * SegmentChunkSize) + 56,
-			want: &CompactMapSegment{
-				list:     []CompactNeedleValue{},
-				chunk:    5,
-				firstKey: MaxCompactKey,
-				lastKey:  0,
-			},
-		},
-	}
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			cs := testMap.segmentForKey(tc.key)
-			if !reflect.DeepEqual(cs, tc.want) {
-				t.Errorf("got segment %v, want %v", cs, tc.want)
-			}
-		})
-	}
-
 	wantMap := &CompactMap{
-		segments: map[int]*CompactMapSegment{
-			0: &CompactMapSegment{
-				list:     []CompactNeedleValue{},
-				chunk:    0,
-				firstKey: MaxCompactKey,
-				lastKey:  0,
-			},
-			1: &CompactMapSegment{
-				list:     []CompactNeedleValue{},
-				chunk:    1,
-				firstKey: MaxCompactKey,
-				lastKey:  0,
-			},
-			5: &CompactMapSegment{
-				list:     []CompactNeedleValue{},
-				chunk:    5,
-				firstKey: MaxCompactKey,
-				lastKey:  0,
-			},
+		values: map[types.NeedleId]CompactNeedleValue{
+			10: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(0)), size: 100},
+			20: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(100)), size: -200},
+			30: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(300)), size: 300},
+			40: CompactNeedleValue{offset: OffsetToCompact(types.Uint32ToOffset(600)), size: -400},
 		},
 	}
 	if !reflect.DeepEqual(testMap, wantMap) {
-		t.Errorf("got map %v, want %v", testMap, wantMap)
+		t.Errorf("got result segment %v, want %v", testMap, wantMap)
 	}
 }
 
@@ -439,7 +225,7 @@ func TestAscendingVisit(t *testing.T) {
 }
 
 func TestOrdering(t *testing.T) {
-	count := 8 * SegmentChunkSize
+	count := 400000
 	keys := []types.NeedleId{}
 	for i := 0; i < count; i++ {
 		keys = append(keys, types.NeedleId(i))
@@ -467,11 +253,5 @@ func TestOrdering(t *testing.T) {
 	})
 	if err != nil {
 		t.Errorf("got error %v, expected none", err)
-	}
-
-	// Given that we've written a integer multiple of SegmentChunkSize, all
-	// segments should be fully utilized and capacity-adjusted.
-	if l, c := cm.Len(), cm.Cap(); l != c {
-		t.Errorf("map length (%d) doesn't match capacity (%d)", l, c)
 	}
 }
